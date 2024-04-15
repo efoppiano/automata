4116a943399df6af434ccb29b75151b3879e034f
efoppiano <efoppiano@fi.uba.ar>
2024-04-14 23:16:09 -0300

    Change indentation

--- src/generator/bbs.py
+++ src/generator/bbs.py
@@ -2,32 +2,32 @@ import sympy
 import numpy as np
 
 class BlumBlumShub:
-  @staticmethod
-  def _next_usable_prime(x: int) -> int:
-    p = sympy.nextprime(x)
-    while (p % 4 != 3):
-        p = sympy.nextprime(p)
-    return p
+    @staticmethod
+    def _next_usable_prime(x: int) -> int:
+        p = sympy.nextprime(x)
+        while (p % 4 != 3):
+                p = sympy.nextprime(p)
+        return p
 
-  # We define two big numbers and find the appropiate
-  # parameters based on them
-  p = _next_usable_prime(8*10**8)
-  q = _next_usable_prime(4*10**8)
-  M = p * q
+    # We define two big numbers and find the appropiate
+    # parameters based on them
+    p = _next_usable_prime(8*10**8)
+    q = _next_usable_prime(4*10**8)
+    M = p * q
 
-  def __init__(self, seed):
-    self._curr = seed
+    def __init__(self, seed):
+        self._curr = seed
 
-  def generate(self, amount: int) -> np.array:
-    arr = [0] * amount
-    for i in range(amount):
-      arr[i] = next(self)
-    return np.array(arr)
+    def generate(self, amount: int) -> np.array:
+        arr = [0] * amount
+        for i in range(amount):
+            arr[i] = next(self)
+        return np.array(arr)
 
-  def __next__(self) -> float:
-    self._curr = pow(self._curr, 2, self.M)
+    def __next__(self) -> float:
+        self._curr = pow(self._curr, 2, self.M)
 
-    return self._curr / self.M
+        return self._curr / self.M
 
-  def __iter__(self):
-    return self
+    def __iter__(self):
+        return self
--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
 
 class Generator(ABC):
-  @abstractmethod
-  def random(self) -> float:
-    pass
+    @abstractmethod
+    def random(self) -> float:
+        pass
 
-  @abstractmethod
-  def randint(self, a: int, b: int) -> int:
-    pass
+    @abstractmethod
+    def randint(self, a: int, b: int) -> int:
+        pass
 
-  @abstractmethod
-  def poi(self, l: float) -> int:
-    pass
+    @abstractmethod
+    def poi(self, l: float) -> int:
+        pass
