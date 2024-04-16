a52e8eda693348654127d0a3d201443c9726fd44
efoppiano <efoppiano@fi.uba.ar>
2024-04-15 21:53:23 -0300

    Add waiting area max size

--- src/waiting_area.py
+++ src/waiting_area.py
@@ -9,15 +9,19 @@ gen = TPGenerator(2**10**7)
 T = TypeVar("T")
 
 class WaitingArea(Generic[T]):
-    def __init__(self, arrival_rate: float, rel_grid: T, pedestrian_sem: Semaphore):
+    def __init__(self, arrival_rate: float, rel_grid: T, pedestrian_sem: Semaphore, max_size: int = 100):
         self._rel_grid = rel_grid
         self._waiting_pedestrians = 0
         self._arrival_rate = arrival_rate
         self._total_generated_pedestrians = 0
         self._pedestrian_sem = pedestrian_sem
+        self._max_size = max_size
 
     def _generate_pedestrians(self):
-        new_pedestrians = gen.poi(self._arrival_rate)
+        if self._waiting_pedestrians == self._max_size:
+            return
+        
+        new_pedestrians = min(self._max_size - self._waiting_pedestrians, gen.poi(self._arrival_rate))
         self._waiting_pedestrians += new_pedestrians
         self._total_generated_pedestrians += new_pedestrians
 
@@ -32,3 +36,6 @@ class WaitingArea(Generic[T]):
         self._generate_pedestrians()
         if self._pedestrian_sem.state == "green":
             self._move_pedestrians()
+
+    def __repr__(self) -> str:
+        return f"{self._waiting_pedestrians : <3}"
