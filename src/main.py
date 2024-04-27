f67f79a511f36cfce01cbcdae87535ebe1cde9a0
efoppiano <efoppiano@fi.uba.ar>
2024-04-27 17:47:54 -0300

    Add animation with matplotlib

--- src/main.py
+++ src/main.py
@@ -6,12 +6,15 @@ def clear_output():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    automata = Automata()
-    for i in range(3600):
+    automata = Automata(animate=True)
+    for i in range(600):
         automata.update()
         clear_output()
         automata.show()
-        time.sleep(0.3)
+        time.sleep(0.2)
+
+    print("Saving animation, this may take a while...")
+    automata.save_mp4("animation2.mp4")
 
 if __name__ == "__main__":
     main()
