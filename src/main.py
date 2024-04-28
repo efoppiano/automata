36f87ef36fe9a46606faae8ec7d5b46962a84e33
efoppiano <efoppiano@fi.uba.ar>
2024-04-28 04:47:38 -0300

    main.py minor fix

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def main():
         time.sleep(0.2)
 
     print("Saving animation, this may take a while...")
-    automata.save_mp4("animation2.mp4")
+    automata.save_mp4("animation.mp4")
 
 if __name__ == "__main__":
     main()
